512bba716d66f5e22a12ce95435b40e30827e4c0
amjad arabia <black_street@hotmail.co.il>
2023-01-24 15:13:43 +0200

    end of section 2

---
+++ API/Controllers/TablesController.cs
@@ -0,0 +1,29 @@
+
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class TablesController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public TablesController(DataContext context)
+        {
+            _context = context;
+
+        }
+    [HttpGet]//api/table someone reach this endpoint 
+    public async Task<ActionResult<List<Table>>>GetTables()
+    {
+        return await _context.Tables.ToListAsync();
+    }
+    [HttpGet("{id}")] //api/tables/id=adao23oisnd
+    public async Task<ActionResult<Table>>GetTable(Guid id)
+    {
+        return await _context.Tables.FindAsync(id);
+    }
+    }
+}
